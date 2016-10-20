040a6eaec8c3ca523b1ce32bf0b119788b8ddb0f
Gerhard Sittig <gerhard.sittig@gmx.net>
2016-10-20 09:23:33 +0200

    license: remove FSF postal address from boiler plate license text
    Remove the FSF postal address as it might change (it did in the past).
    Reference the gnu.org website instead which is more stable.

--- fx2lib/include/i2c.h
+++ fx2lib/include/i2c.h
@@ -11,8 +11,7 @@
 // Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+// License along with this library; if not, see <http://www.gnu.org/licenses/>.
 
 /** \file i2c.h 
  *  Utilities for reading and writing to i2c devices and as eeproms.
--- fx2lib/include/serial.h
+++ fx2lib/include/serial.h
@@ -11,8 +11,7 @@
 // Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+// License along with this library; if not, see <http://www.gnu.org/licenses/>.
 
 /** \file serial.h
  * defines functions to print to a serial console with SIO0
