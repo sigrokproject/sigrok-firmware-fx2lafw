14d3dfb79dca9e261dbebd5a381155e5a93ce0e0
Gerhard Sittig <gerhard.sittig@gmx.net>
2017-12-29 16:05:04 +0100

    fx2lafw: fixup missing semicolon after macro "function call"
    Since RESETFIFO() macro currently is "phrased unfortunately", the call
    site got away with the omission of a semicolon. Adding the missing
    semicolon does not harm the current implementation, and unbreaks the
    build when RESETFIFO() gets improved in the future.

--- fx2lafw.c
+++ fx2lafw.c
@@ -78,7 +78,7 @@ static void setup_endpoints(void)
 
 	/* EP2: Reset the FIFOs. */
 	/* Note: RESETFIFO() gets the EP number WITHOUT bit 7 set/cleared. */
-	RESETFIFO(0x02)
+	RESETFIFO(0x02);
 
 	/* EP2: Enable AUTOIN mode. Set FIFO width to 8bits. */
 	EP2FIFOCFG = bmAUTOIN;
